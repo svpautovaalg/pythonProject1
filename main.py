cabce190c459f87b45af5c8ab77da2ab324d5816
svpautova <svpautova@miem.hse.ru>
2022-04-23 17:13:59 +0300

    add egor

--- main.py
+++ main.py
@@ -12,6 +12,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('что хотите hi1 test')
-    print("add egor")
+    print("add egor 1")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
